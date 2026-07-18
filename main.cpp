d01108febd55222db4326ec915e7191f0717d99a
Muhammad Nauman <maliknomigep@gmail.com>
2026-07-18 13:57:02 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void changeArr(int arr[], int size){
+    cout<<"in function\n";
+   for(int i=0; i<size; i++){
+    arr[i] = 2*arr[i];
+     cout<<arr[i]<<" ";
+   }
+}
+
+int main()
+{
+    int arr[] = {1,2,3};
+    cout<<"in main\n";
+    for(int i=0; i<3; i++){
+            cout<<arr[i]<<" ";
+
+    }
+    cout<<endl;
+
+
+    changeArr(arr,3);
+
+
+    return 0;
+}
